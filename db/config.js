bc40f95b6d4e81d80bf5e69967745426577cd8e1
facubrusa <facundobrusa11@gmail.com>
2022-06-22 21:08:23 -0300

    fix config when run migration

--- db/config.js
+++ db/config.js
@@ -1,16 +1,9 @@
 const { config } = require('../config/config');
-
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
 module.exports = {
-    development: {
-        url: URI,
-        dialect: 'postgres',
-    },
+    development: { url: config.dbUrl, dialect: 'postgres' },
     production: {
-        url: URI,
+        url: config.dbUrl,
         dialect: 'postgres',
-    }
-}
+        dialectOptions: { ssl: { rejectUnauthorized: false } },
+    },
+};
